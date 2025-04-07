f24c6d7be8226190327160f74f2681eae3c640ce
Lukas2094 <lukas2_8@hotmail.com>
2025-04-07 16:51:42 -0300

    add logo img

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -30,6 +30,10 @@ const LoginPage = () => {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <div className='w-full flex items-center justify-center'>
+          <img src="\img\magnum.png" alt="magnum" className='w-1/3 rounded-md' />  
+        </div>
+        
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
           Acesse sua conta
         </h2>
