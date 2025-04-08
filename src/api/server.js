7b614fc491a7c2d7a194c078d0779861a21c9edd
Lukas2094 <lukas2_8@hotmail.com>
2025-04-07 21:16:03 -0300

    update server

--- src/api/server.js
+++ src/api/server.js
@@ -1,16 +1,10 @@
 const jsonServer = require('json-server');
-const auth = require('json-server-auth');
-const path = require('path');
-
 const server = jsonServer.create();
+const path = require('path');
 const router = jsonServer.router(path.join(__dirname, '..', 'db.json'));
 const middlewares = jsonServer.defaults();
 
-server.db = router.db;
-
 server.use(middlewares);
-server.use(jsonServer.bodyParser);
-server.use(auth);
 server.use(router);
 
 module.exports = server;
