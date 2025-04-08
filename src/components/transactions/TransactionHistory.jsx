45bae59ace77de59561c2cb89dc95143d04bfa83
Lukas2094 <lukas2_8@hotmail.com>
2025-04-07 22:05:27 -0300

    remove logs

--- src/components/transactions/TransactionHistory.jsx
+++ src/components/transactions/TransactionHistory.jsx
@@ -98,8 +98,7 @@ const TransactionHistory = ({ transactions = [] }) => {
               const { icon, bgColor } = getTransactionIcon(transaction.type, transaction.value);
               const isNegative = transaction.value < 0;
               const absValue = Math.abs(transaction.value);
-              console.log(transaction);
-              
+  
               return (
                 <li key={transaction.id} className="p-4 hover:bg-gray-50 transition-colors">
                   <div className="flex items-center justify-between">
